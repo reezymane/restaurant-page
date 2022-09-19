40b04c874a4b561899337545bfe7dd8cb3672856
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-19 12:04:19 -0500

    Add eventlisteners for buttons and logic to clear current page content

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import './style.css';
 import {initialPage} from './initialPage';
 import {menu} from './menu';
 
-initialPage();
+initialPage;
+
+// Clears current page contents and adds appropriate tab content
+const navButtons = document.querySelectorAll('button');
+navButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const mainContents = document.getElementsByClassName('mainContents');
+        
+        while(mainContents.item(0).firstChild != null) {
+            mainContents.item(0).removeChild(mainContents.item(0).firstChild);
+        };
+    });
+});
