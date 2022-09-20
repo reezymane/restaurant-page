bfc334d3d828bdc60e76462d6ee32860f7dce0c4
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-20 15:16:09 -0500

    Change property to borderBottom in button if statement

--- src/initialPage.js
+++ src/initialPage.js
@@ -3,14 +3,14 @@ import logo from './img/logo.png';
 const initialPage = () => {
     const content = document.getElementById('content');
 
-    // Add logo to contents div
+// Add logo to contents div
     const myLogo = new Image();
     myLogo.src = logo;
     myLogo.classList.add('logo');
     
     content.appendChild(myLogo);
 
-    // Add description/history to mainContents
+// Add description/history to mainContents
     const description = document.createElement('p');
     description.textContent = 'World Class Southern-Cajun Cuisine';
     description.classList.add('restDescription');
@@ -23,12 +23,13 @@ const initialPage = () => {
 
     content.appendChild(history);
 
+// Adjusts button underline for current tab
     const button = document.querySelectorAll('button');
     button.forEach((button) => {
         if (button.textContent === 'Home') {
             button.style.borderBottom = '2px solid rgb(254, 244, 230)'
         } else {
-            button.style.property = 'none';
+            button.style.borderBottom = 'none';
         };
     });
 };
