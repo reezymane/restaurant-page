09d3bb0cfd0aff9663a550ae6acb6822fa9d8fdd
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-20 14:42:22 -0500

    Add class to restaurant description and history

--- src/initialPage.js
+++ src/initialPage.js
@@ -13,11 +13,13 @@ const initialPage = () => {
     // Add description/history to mainContents
     const description = document.createElement('p');
     description.textContent = 'World Class Southern-Cajun Cuisine';
+    description.classList.add('restDescription');
 
     content.appendChild(description);
 
     const history = document.createElement('p');
     history.textContent = 'Been Bussin Since 1492';
+    history.classList.add('history');
 
     content.appendChild(history);
 };
